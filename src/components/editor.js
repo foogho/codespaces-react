3254203f795745acbc7fed715cf0569c9969b411
Ali Ebrahimi <57700091+foogho@users.noreply.github.com>
2022-11-18 12:05:30 +0330

    Update editor.js
    -remove useless contructor
    
    -ci servers treat warnings as errors

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 export default class Editor extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   componentDidMount() {
     // on initial load textarea onChange event doesn't raise so we should
     // manually call onMarkdownChange callback
